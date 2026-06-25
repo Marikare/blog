358832c208651a53476f53cda202e56cd9d07cf1
Marina <marinaiokoiama@hotmail.com>
2026-06-25 19:29:22 -0300

    blog

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Contador from "./Contador";
+
+function App() {
+  return (
+    <div>
+      <h1>ESSE CONTADOR FOI FEITO COM REACT</h1>
+      <p>CONTADOR AQUI</p>
+      <Contador/>
+    </div>
+  );
+}
+
+export default App;
